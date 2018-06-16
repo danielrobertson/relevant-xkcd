38036528000de9596558933cfe3eda7a7a96db67
Daniel Robertson <danielrobertson733@gmail.com>
2018-06-16 13:37:00 -0500

    search bar prototype

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import {
+  Grid,
+  FormGroup,
+  FormControl,
+  Jumbotron,
+  Button
+} from "react-bootstrap";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Grid>
+          <Jumbotron>
+            <h2>There's always a relevant Xkcd!</h2>
+            <p>
+              With over two thousand comics, there is a relevant Xkcd for every
+              situation - search to see for yourself!
+            </p>
+
+            <div style={{ maxWidth: 400, margin: "0 auto 10px" }}>
+              <form>
+                <FormGroup bsSize="large">
+                  <FormControl type="text" placeholder="Atheists" />
+                </FormGroup>
+              </form>
+
+              <Button bsStyle="primary" bsSize="large" block>
+                Search
+              </Button>
+            </div>
+          </Jumbotron>
+        </Grid>
       </div>
     );
   }
