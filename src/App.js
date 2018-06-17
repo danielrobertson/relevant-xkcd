d7c2e662f04d30d2ac5bcf68f3d568359296c4a0
Daniel Robertson <danielrobertson733@gmail.com>
2018-06-17 02:01:55 -0500

    new ui prototype with centered search bar

--- src/App.js
+++ src/App.js
@@ -9,29 +9,39 @@ import {
 } from "react-bootstrap";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="App">
-        <Grid>
-          <Jumbotron>
-            <h2>There's always a relevant Xkcd!</h2>
-            <p>
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 class="display-4">Relevant Xkcd</h1>
+            <p class="lead">
               With over two thousand comics, there is a relevant Xkcd for every
               situation - search to see for yourself!
             </p>
-
-            <div style={{ maxWidth: 400, margin: "0 auto 10px" }}>
-              <form>
-                <FormGroup bsSize="large">
-                  <FormControl type="text" placeholder="Atheists" />
-                </FormGroup>
-              </form>
-
-              <Button bsStyle="primary" bsSize="large" block>
-                Search
-              </Button>
+          </div>
+        </div>
+        <Grid>
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="input-group input-group-lg">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Search for..."
+                />
+                <span class="input-group-btn">
+                  <button class="btn btn-primary" type="button">
+                    Go!
+                  </button>
+                </span>
+              </div>
             </div>
-          </Jumbotron>
+          </div>
         </Grid>
       </div>
     );
