481b9658036c2896803ca5798b02ce77d725e857
Daniel Robertson <danielrobertson733@gmail.com>
2020-02-24 18:23:03 -0600

    hit appbase elastic search api

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,5 +1,41 @@
-export default (req, res) => {
+import Appbase from "appbase-js";
+
+const elastic = new Appbase({
+  url: process.env.APPBASE_URL,
+  app: "relevant-xkcd",
+  credentials: process.env.APPBASE_KEY
+});
+
+const search = keyword => {
+  return new Promise((resolve, reject) => {
+    elastic
+      .search({
+        type: "comics",
+        body: {
+          query: {
+            query_string: {
+              query: keyword
+            }
+          }
+        }
+      })
+      .on("data", res => {
+        resolve(res);
+      })
+      .on("error", err => {
+        reject(err);
+      });
+  });
+};
+
+export default async (req, res) => {
   const { q } = req.query;
-  console.log("api " + q);
-  res.status(200).json({ results: "foobar" });
+
+  try {
+    const { hits } = await search(q);
+    res.status(200).json(hits);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 };
