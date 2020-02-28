f7fc62005e38dfc215c7c02046ee903ba9fa23e0
Daniel Robertson <danielrobertson733@gmail.com>
2020-02-27 23:46:52 -0600

    optional chaining to fix build, guessing there's an underlying issue or misconception

--- pages/components/Grid.js
+++ pages/components/Grid.js
@@ -1,6 +1,6 @@
 import Tile from "./Tile";
 
 export default function Grid({ comics }) {
-  const tiles = comics.map(c => <Tile key={c._id} comic={c} />);
+  const tiles = comics?.map(c => <Tile key={c._id} comic={c?._source} />);
   return <div>{tiles}</div>;
 }
--- pages/components/Tile.js
+++ pages/components/Tile.js
@@ -1,11 +1,11 @@
 import { PseudoBox, Image, Link } from "@chakra-ui/core";
 
-export default function ComicTile({ comic }) {
-  const url = `https://xkcd.com/${comic._source.num}`;
+export default function Tile({ comic }) {
+  const url = `https://xkcd.com/${comic?.num}`;
   return (
     <PseudoBox m="15px" maxW="md" _hover={{ shadow: "2xl" }}>
       <Link href={url} isExternal="true">
-        <Image src={comic._source.img} alt={comic._source.title} />
+        <Image src={comic?.img} alt={comic?.title} />
       </Link>
     </PseudoBox>
   );
