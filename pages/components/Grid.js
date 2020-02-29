15e471ab16b9d79671cf211a075cfa9464f6fae0
Daniel Robertson <danielrobertson733@gmail.com>
2020-02-28 19:32:55 -0600

    prevent pre-rendering on search result components

--- pages/components/Grid.js
+++ pages/components/Grid.js
@@ -1,6 +1,8 @@
 import Tile from "./Tile";
 
 export default function Grid({ comics }) {
-  const tiles = comics?.map(c => <Tile key={c._id} comic={c?._source} />);
-  return <div>{tiles}</div>;
+  const tiles = comics.map(c => <Tile key={c._id} comic={c?._source} />);
+  return tiles.length > 0 ? <div>{tiles}</div> : <div></div>;
 }
+
+Grid.getInitialProps = () => {};
